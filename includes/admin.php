968f5cb99cbf199542e825798b88ceb82366ba5c
Pippin Williamson <pippin@pippinsplugins.com>
2013-07-22 02:07:21 -0500

    Almost fully working

--- includes/admin.php
+++ includes/admin.php
@@ -156,241 +156,258 @@ function edd_process_csv_import() {
 
 	$defaults = array(
 		'post_name'		=> '',
+		'post_date'		=> '',
 		'post_author'	=> '',
 		'post_title'	=> '',
 		'post_content'	=> '',
 		'post_excerpt'	=> '',
-		'post_status'	=> '',
+		'post_status'	=> 'draft',
 		'post_type'		=> 'download',
 		'categories'	=> '',
-		'tags'			=> ''
+		'tags'			=> '',
+		'_edd_price'=> '',
+		'_edd_files'=> '',
+		'_edd_download_limit'=> '',
+		'_edd_button_behavior'=> '',
+		'_edd_hide_purchase_link'=> '',
+		'_thumbnail_id'=> '',
 	);
 
 	$csv_fields  = maybe_unserialize( get_transient( 'csv_fields' ) );
 	$csv_fields  = wp_parse_args( $csv_fields, $defaults );
 	$headers     = get_transient( 'edd_csv_headers' );
-	$file_errors = array();
-
-	//echo '<pre>'; print_r( $csv_fields ); echo '</pre>';
-
-	ini_set( 'auto_detect_line_endings', true );
 
 	$filename = get_transient( 'edd_csv_file' );
-	$file = trailingslashit( WP_CONTENT_DIR ) . $filename;
-	if( ( $handle = fopen( $file, 'r' ) ) !== FALSE ) {
+	$import_file = trailingslashit( WP_CONTENT_DIR ) . $filename;
+
+	$csv = new parseCSV();
+
+	// Detect deliminator
+	$csv->auto( $import_file );
+
+	//echo $csv_fields['post_author']; exit;
+
+	// Map headers to post fields
+	$post_name_key		= array_search( $csv_fields['post_name'], $headers );
+	$post_author_key	= array_search( $csv_fields['post_author'], $headers );
+	$post_title_key		= array_search( $csv_fields['post_title'], $headers );
+	$post_content_key	= array_search( $csv_fields['post_content'], $headers );
+	$post_excerpt_key	= array_search( $csv_fields['post_excerpt'], $headers );
+	$post_status_key	= array_search( $csv_fields['post_status'], $headers );
+	$post_date_key		= array_search( $csv_fields['post_date'], $headers );
+
+	// Meta fields
+	$price_key			= array_search( $csv_fields['_edd_price'], $headers );
+	$dl_limit_key		= array_search( $csv_fields['_edd_download_limit'], $headers );
+	$button_behavior	= array_search( $csv_fields['_edd_button_behavior'], $headers );
+	$hide_link			= array_search( $csv_fields['_edd_hide_purchase_link'], $headers );
+
+	// Categories
+	$categories_key		= array_search( $csv_fields['categories'], $headers );
+
+	// Tags
+	$tags_key			= array_search( $csv_fields['tags'], $headers );
+
+	// Files
+	$files_key			= array_search( $csv_fields['_edd_files'], $headers );
+
+	// Featured image
+	$image_key			= array_search( $csv_fields['_thumbnail_id'], $headers );
+	foreach ( $csv->data as $key => $row ) {
+
+		$new_row = array();
 		$i = 0;
-		while( ( $row = fgetcsv( $handle, 1000 ) ) !== FALSE ) {
+		foreach( $row as $column ) {
+			$new_row[ $i ] = $column;
+			$i++;
+		}
 
-			// Get the column keys
-			if( $i < 1 ) {
-				// Default fields
-				$post_name_key		= array_search( $csv_fields['post_name'], $row );
-				$post_author_key	= array_search( $csv_fields['post_author'], $row );
-				$post_title_key		= array_search( $csv_fields['post_title'], $row );
-				$post_content_key	= array_search( $csv_fields['post_content'], $row );
-				$post_excerpt_key	= array_search( $csv_fields['post_excerpt'], $row );
-				$post_status_key	= array_search( $csv_fields['post_status'], $row );
-				$post_date_key		= array_search( $csv_fields['post_date'], $row );
+		// Get the column keys
 
-				// Meta fields
-				$price_key			= array_search( $csv_fields['_edd_price'], $row );
-				$dl_limit_key		= array_search( $csv_fields['_edd_download_limit'], $row );
-				$button_behavior	= array_search( $csv_fields['_edd_button_behavior'], $row );
-				$hide_link			= array_search( $csv_fields['_edd_hide_purchase_link'], $row );
+		$post_data = array(
+			'post_name'		=> $new_row[ $post_name_key ],
+			'post_author'	=> $new_row[ $post_author_key ],
+			'post_title'	=> $new_row[ $post_title_key ],
+			'post_content'	=> $new_row[ $post_content_key ],
+			'post_excerpt'	=> $new_row[ $post_name_key ],
+			'post_status'	=> $new_row[ $post_status_key ],
+			'post_date'		=> date( 'Y-m-d H:i:s', strtotime( $new_row[ $post_date_key ] ) ),
+			'post_type'		=> 'download'
+		);
 
-				// Categories
-				$categories_key		= array_search( $csv_fields['categories'], $row );
 
-				// Tags
-				$tags_key			= array_search( $csv_fields['tags'], $row );
+		$file_errors = array();
 
-				// Files
-				$files_key			= array_search( $csv_fields['_edd_files'], $row );
+		// Set files
+		if( $files_key && !empty( $new_row[ $files_key ] ) ) {
 
-				// Featured image
-				$image_key			= array_search( $csv_fields['_thumbnail_id'], $row );
-			}
+			$files = array_map( 'trim', explode( '|', $new_row[ $files_key ] ) );
+			$final_files = array();
+			$file_path = '';
+			// Make sure files exist
+			foreach( $files as $file ) {
+				$file_details = parse_url( $file );
+				if( ! $file_details || ! isset( $file_details['scheme'] ) || 'http' != $file_details['scheme'] || 'https' != $file_details['scheme'] ) {
+					// Set preferred path for file hosting
+					$search_base_path = trailingslashit( WP_CONTENT_DIR );
+					$preferred_path = $search_base_path . 'uploads/edd/' . $file;
 
- 			if( ! $headers && $i <= 1 ) {
- 				$i++;
- 				continue;
- 			}
-
- 			if( ( $headers && $i > 0 ) || ! $headers ) {
-				$post_data = array(
-					'post_name'		=> $row[ $post_name_key ],
-					'post_author'	=> $row[ $post_author_key ],
-					'post_title'	=> $row[ $post_title_key ],
-					'post_content'	=> $row[ $post_content_key ],
-					'post_excerpt'	=> $row[ $post_name_key ],
-					'post_status'	=> $row[ $post_status_key ],
-					'post_date'		=> date( 'Y-m-d H:i:s', strtotime( $row[ $post_date_key ] ) ),
-					'post_type'		=> 'download'
-				);
-
-				// Set files
-				if( $files_key && !empty( $row[ $files_key ] ) ) {
-
-					$files = array_map( 'trim', explode( '|', $row[ $files_key ] ) );
-					$final_files = array();
-
-					// Make sure files exist
-					foreach( $files as $file ) {
-						$file_details = parse_url( $file );
-						if( ! $file_details || ! isset( $file_details['scheme'] ) || 'http' != $file_details['scheme'] || 'https' != $file_details['scheme'] ) {
-							// Set preferred path for file hosting
-							$search_base_path = trailingslashit( WP_CONTENT_DIR );
-							$preferred_path = $search_base_path . 'uploads/edd/' . $file;
-
-							if( file_exists( $preferred_path ) ) {
-								// Check /wp-content/uploads/edd/$file
-								$file_path = $preferred_path;
-							} elseif( file_exists( $search_base_path . $file ) ) {
-								// Check /wp-content/$file
-								if( move_uploaded_file( $search_base_path . $file, $preferred_path ) ) {
-									$file_path = $preferred_path;
-								} else {
-									$file_path = $search_base_path . $file;
-								}
-							} elseif( file_exists( $search_base_path . 'uploads/' . $file ) ) {
-								// Check /wp-content/uploads/$file
-								if( move_uploaded_file( $search_base_pat . 'uploads/' . $file, $preferred_path ) ) {
-									$file_path = $preferred_path;
-								} else {
-									$file_path = $search_base_path . 'uploads/' . $file;
-								}
-							} else {
-								// Error
-								$file_errors[] = array(
-									'row'  => $i + 1,
-									'file' => $file
-								);
-							}
+					if( file_exists( $preferred_path ) ) {
+						// Check /wp-content/uploads/edd/$file
+						$file_path = $preferred_path;
+					} elseif( file_exists( $search_base_path . $file ) ) {
+						// Check /wp-content/$file
+						if( move_uploaded_file( $search_base_path . $file, $preferred_path ) ) {
+							$file_path = $preferred_path;
+						} else {
+							$file_path = $search_base_path . $file;
 						}
-
-						// Store file in array for use later
-						$final_files[] = array(
-							'name' => basename( $file_path ),
-							'file' => str_replace( WP_CONTENT_DIR, WP_CONTENT_URL, $file_path )
+					} elseif( file_exists( $search_base_path . 'uploads/' . $file ) ) {
+						// Check /wp-content/uploads/$file
+						if( move_uploaded_file( $search_base_path . 'uploads/' . $file, $preferred_path ) ) {
+							$file_path = $preferred_path;
+						} else {
+							$file_path = $search_base_path . 'uploads/' . $file;
+						}
+					} else {
+						// Error
+						$file_errors[] = array(
+							'row'  => $i + 1,
+							'file' => $file
 						);
 					}
 				}
 
-				if( ! empty( $file_errors ) ) {
-					$i++;
-					continue;
-				}
+				// Store file in array for use later
+				$final_files[] = array(
+					'name' => basename( $file_path ),
+					'file' => str_replace( WP_CONTENT_DIR, WP_CONTENT_URL, $file_path )
+				);
+			}
+		}
 
-				$image = false;
-				// Set featured images
-				if( $image_key && !empty( $row[ $image_key ] ) ) {
+		if( ! empty( $file_errors ) ) {
+			$i++;
+			continue;
+		}
 
-					require_once ABSPATH . 'wp-adminincludes/image.php';
-					require_once ABSPATH . 'wp-adminincludes/file.php';
-					require_once ABSPATH . 'wp-adminincludes/media.php';
 
-					$image = true;
-					$image_details = parse_url( $image_key );
-					if( ! $image_details || ! isset( $image_details['scheme'] ) || 'http' != $image_details['scheme'] || 'https' != $image_details['scheme'] ) {
-						// Set preferred path for file hosting
-						$search_base_path = trailingslashit( WP_CONTENT_DIR );
-						$preferred_path = $search_base_path . 'uploads/edd/' . $image_key;
 
-						if( file_exists( $preferred_path ) ) {
-							// Check /wp-content/uploads/edd/$file
-							$file_path = $preferred_path;
-						} elseif( file_exists( $search_base_path . $image_key ) ) {
-							// Check /wp-content/$file
-							if( move_uploaded_file( $search_base_path . $image_key, $preferred_path ) ) {
-								$file_path = $preferred_path;
-							} else {
-								$file_path = $search_base_path . $image_key;
-							}
-						} elseif( file_exists( $search_base_path . 'uploads/' . $image_key ) ) {
-							// Check /wp-content/uploads/$file
-							if( move_uploaded_file( $search_base_pat . 'uploads/' . $image_key, $preferred_path ) ) {
-								$file_path = $preferred_path;
-							} else {
-								$file_path = $search_base_path . 'uploads/' . $image_key;
-							}
-						} else {
-							// Error
-							$image = false;
-							$file_errors[] = array(
-								'row'  => $i + 1,
-								'file' => $image_key
-							);
-						}
+		$image = false;
+
+		// TODO: doesn't work
+
+		// Set featured images
+		if( $image_key && !empty( $new_row[ $image_key ] ) ) {
+
+			require_once ABSPATH . 'wp-adminincludes/image.php';
+			require_once ABSPATH . 'wp-adminincludes/file.php';
+			require_once ABSPATH . 'wp-adminincludes/media.php';
+
+			$image = true;
+			$image_details = parse_url( $image_key );
+			if( ! $image_details || ! isset( $image_details['scheme'] ) || 'http' != $image_details['scheme'] || 'https' != $image_details['scheme'] ) {
+				// Set preferred path for file hosting
+				$search_base_path = trailingslashit( WP_CONTENT_DIR );
+				$preferred_path = $search_base_path . 'uploads/edd/' . $image_key;
+
+				if( file_exists( $preferred_path ) ) {
+					// Check /wp-content/uploads/edd/$file
+					$file_path = $preferred_path;
+				} elseif( file_exists( $search_base_path . $image_key ) ) {
+					// Check /wp-content/$file
+					if( move_uploaded_file( $search_base_path . $image_key, $preferred_path ) ) {
+						$file_path = $preferred_path;
+					} else {
+						$file_path = $search_base_path . $image_key;
 					}
-
-					// Store file in array for use later
-					$final_files[] = array(
-						'name' => basename( $file_path ),
-						'file' => str_replace( WP_CONTENT_DIR, WP_CONTENT_URL, $file_path )
+				} elseif( file_exists( $search_base_path . 'uploads/' . $image_key ) ) {
+					// Check /wp-content/uploads/$file
+					if( move_uploaded_file( $search_base_path . 'uploads/' . $image_key, $preferred_path ) ) {
+						$file_path = $preferred_path;
+					} else {
+						$file_path = $search_base_path . 'uploads/' . $image_key;
+					}
+				} else {
+					// Error
+					$image = false;
+					$file_errors[] = array(
+						'row'  => $i + 1,
+						'file' => $image_key
 					);
 				}
+			}
 
-				// Check /wp-content/uploads/edd/$file
-				$post_id = wp_insert_post( $post_data );
+			// Store file in array for use later
+			$final_files[] = array(
+				'name' => basename( $file_path ),
+				'file' => str_replace( WP_CONTENT_DIR, WP_CONTENT_URL, $file_path )
+			);
+		}
 
-				// Make sure it was created
-				if( $post_id ) {
 
-					// Set meta fields
-					if( $price_key && !empty( $row[ $price_key ] ) )
-						update_post_meta( $post_id, 'edd_price', $row[ $price_key ] );
+		// Check /wp-content/uploads/edd/$file
+		$post_id = wp_insert_post( $post_data );
 
-					if( $dl_limit_key && !empty( $row[ $dl_limit_key ] ) )
-						update_post_meta( $post_id, '_edd_download_limit', $row[ $dl_limit_key ] );
+		// Make sure it was created
+		if( $post_id ) {
 
-					if( $button_behavior && !empty( $row[ $button_behavior ] ) )
-						update_post_meta( $post_id, '_edd_button_behavior', $row[ $button_behavior ] );
+			// Set meta fields
+			if( $price_key && !empty( $new_row[ $price_key ] ) )
+				update_post_meta( $post_id, 'edd_price', $new_row[ $price_key ] );
 
-					if( $hide_link && !empty( $row[ $hide_link ] ) )
-						update_post_meta( $post_id, '_edd_hide_purchase_link', $row[ $hide_link ] );
+			if( $dl_limit_key && !empty( $new_row[ $dl_limit_key ] ) )
+				update_post_meta( $post_id, '_edd_download_limit', $new_row[ $dl_limit_key ] );
 
-					// Set download files
-					if( ! empty( $final_files ) )
-						update_post_meta( $post_id, 'edd_download_files', $final_files );
+			if( $button_behavior && !empty( $new_row[ $button_behavior ] ) )
+				update_post_meta( $post_id, '_edd_button_behavior', $new_row[ $button_behavior ] );
 
-					// Attach image
-					if( $image )
-						$attachment_id = wp_handle_upload( $file_path, $post_id );
+			if( $hide_link && !empty( $new_row[ $hide_link ] ) )
+				update_post_meta( $post_id, '_edd_hide_purchase_link', $new_row[ $hide_link ] );
 
-					// Set tags
-					if( $tags_key && !empty( $row[ $tags_key ] ) ) {
+			// Set download files
+			if( ! empty( $final_files ) )
+				update_post_meta( $post_id, 'edd_download_files', $final_files );
 
-						$tags = array_map( 'trim', explode( '|', $row[ $tags_key ] ) );
+			// Attach image
+			if( $image ) {
+				$attachment_id = wp_handle_upload( $file_path, $post_id );
+				update_post_meta( $post_id, '_thumbnail_id', $attachment_id );
 
-						// Create tags if they don't exist
-						foreach( $tags as $tag ) {
-							if( !term_exists( $tag, 'download_tag' ) ) {
-								wp_insert_term( $tag, 'download_tag' );
-							}
-						}
+			}
 
-						wp_set_object_terms( $post_id, $tags, 'download_tag' );
-					}
+			// Set tags
+			if( $tags_key && !empty( $new_row[ $tags_key ] ) ) {
 
-					// Set categories
-					if( $categories_key && !empty( $row[ $categories_key ] ) ) {
+				$tags = array_map( 'trim', explode( '|', $new_row[ $tags_key ] ) );
 
-						$categories = array_map( 'trim', explode( '|', $row[ $categories_key ] ) );
-
-						// Create categories if they don't exist
-						foreach( $categories as $category ) {
-							if( !term_exists( $category, 'download_category' ) ) {
-								wp_insert_term( $category, 'download_category' );
-							}
-						}
-
-						wp_set_object_terms( $post_id, $categories, 'download_category' );
+				// Create tags if they don't exist
+				foreach( $tags as $tag ) {
+					if( !term_exists( $tag, 'download_tag' ) ) {
+						wp_insert_term( $tag, 'download_tag' );
 					}
 				}
+
+				wp_set_object_terms( $post_id, $tags, 'download_tag' );
 			}
-			$i++;
+
+			// Set categories
+			if( $categories_key && !empty( $new_row[ $categories_key ] ) ) {
+
+				$categories = array_map( 'trim', explode( '|', $new_row[ $categories_key ] ) );
+
+				// Create categories if they don't exist
+				foreach( $categories as $category ) {
+					if( !term_exists( $category, 'download_category' ) ) {
+						wp_insert_term( $category, 'download_category' );
+					}
+				}
+
+				wp_set_object_terms( $post_id, $categories, 'download_category' );
+			}
 		}
-		fclose( $handle );
+
+
 
 		if( ! empty( $file_errors ) ) {
 			$file_errors = serialize( $file_errors );
@@ -399,11 +416,8 @@ function edd_process_csv_import() {
 			wp_redirect( add_query_arg( array( 'step' => '1', 'errno' => '3' ) ) );
 			exit;
 		}
-
-		exit;
 	}
-
-	ini_set( 'auto_detect_line_endings', false );
+	exit;
 
 }
 
