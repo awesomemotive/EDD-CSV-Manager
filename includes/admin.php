11625a76a8ae66d475c20e68f6546e77689e4b99
Ghost1227 <dgriffiths@ghost1227.com>
2013-07-25 23:53:32 +0000

    Added exporter... not 100% functional yet :(

--- includes/admin.php
+++ includes/admin.php
@@ -14,6 +14,32 @@ if( !defined( 'ABSPATH' ) ) exit;
 
 
 /**
+ * Exporter metabox
+ *
+ * @since		1.0.0
+ * @return		void
+ */
+function edd_csv_exporter_metabox() { 
+	?>
+	<div class="postbox">
+		<h3><span><?php _e( 'Export Products to CSV', 'edd-csv-importer' ); ?></span></h3>
+		<div class="inside">
+			<p><?php _e( 'Export products from your Easy Digital Downloads site to a .csv file.', 'edd-csv-importer' ); ?></p>
+			<form method="post" enctype="multipart/form-data" action="<?php echo admin_url( 'tools.php?page=edd-settings-export-import' ); ?>">
+				<?php
+					echo '<p><input type="hidden" name="edd_action" value="export_csv" />';
+					submit_button( __( 'Export', 'edd' ), 'secondary', 'submit', false );
+					echo '</p>';
+				?>
+			</form>
+		</div>
+	</div>
+<?php
+}
+add_action( 'edd_export_import_top', 'edd_csv_exporter_metabox' );
+
+
+/**
  * Importer metabox
  *
  * @since		1.0.0
@@ -79,6 +105,138 @@ function edd_csv_importer_metabox() {
 	<?php
 }
 add_action( 'edd_export_import_top', 'edd_csv_importer_metabox' );
+
+
+/**
+ * Export products to a CSV file
+ *
+ * @since		1.0.0
+ * @return		void
+ */
+function edd_export_csv() {
+	// Make sure we're allowed to export...
+	if( !current_user_can( 'manage_options' ) )
+		wp_die( __( 'You do not have permission to export data.', 'edd-csv-importer' ), __( 'Error', 'edd-csv-importer' ) );
+
+	// Set CSV header row data
+	$headers = array(
+		'post_id',
+		'post_name',
+		'post_date',
+		'post_author',
+		'post_title',
+		'post_content',
+		'post_excerpt',
+		'post_status',
+		'categories',
+		'tags',
+		'_edd_price',
+		'_edd_files',
+		'_edd_download_limit',
+		'_edd_button_behavior',
+		'_edd_hide_purchase_link',
+		'_edd_images'
+	);
+
+	$headers = apply_filters( 'edd_csv_export_headers', $headers );
+
+	$data[] = $headers;
+
+	// Get our list of downloads!
+	$downloads = get_posts( array( 'post_type' => 'download', 'posts_per_page' => -1 ) );
+
+	foreach( $downloads as $download ) {
+		$post_id = $download->ID;
+		$post_name = $download->post_name;
+		$post_date = $download->post_date;
+		$post_author = $download->post_author;
+		$post_title = $download->post_title;
+		$post_content = $download->post_content;
+		$post_excerpt = $download->post_excerpt;
+		$post_status = $download->post_status;
+
+		$categories_array = wp_get_object_terms( $download->ID, 'download_category', array( 'fields' => 'names' ) );
+		$categories = '';
+		if( count( $categories_array ) == 1 ) {
+			$categories = $categories_array[0];
+		} elseif( count( $categories_array ) > 1 ) {
+			$categories = implode( '|', $categories_array );
+		}
+
+		$tags_array = wp_get_object_terms( $download->ID, 'download_tag', array( 'fields' => 'names' ) );
+		$tags = '';
+		if( count( $tags_array ) == 1 ) {
+			$tags = $tags_array[0];
+		} elseif( count( $tags_array ) > 1 ) {
+			$tags = implode( '|', $tags_array );
+		}
+
+		$price = get_post_meta( $download->ID, 'edd_price', true );
+
+		$files_array = get_post_meta( $download->ID, 'edd_download_files' );
+		$files = '';
+		$download_array = array();
+		if( count( $files_array[0] ) == 1 ) {
+			$files = basename( $files_array[0][0]['file'] );
+			$download_array[] = $files_array[0][0]['file'];
+		} elseif( count( $files_array[0] ) > 1 ) {
+			foreach( $files_array[0] as $file ) {
+				$file_array[] = basename( $file['file'] );
+				$download_array[] = $file['file'];
+			}
+			$files = implode( '|', $file_array );
+		}
+
+		$download_limit = get_post_meta( $download->ID, '_edd_download_limit', true );
+		$button_behavior = get_post_meta( $download->ID, '_edd_button_behavior', true );
+		$hide_purchase_link = get_post_meta( $download->ID, '_edd_hide_purchase_link', true );
+		//$images =
+
+		$row = array(
+			$post_id,
+			$post_name,
+			$post_date,
+			$post_author,
+			$post_title,
+			$post_content,
+			$post_excerpt,
+			$post_status,
+			$categories,
+			$tags,
+			$price,
+			$files,
+			$download_limit,
+			$button_behavior,
+			$hide_purchase_link,
+			$images
+		);
+		$row = apply_filters( 'edd_csv_export_row', $row );
+
+		$data[] = $row;
+	}
+
+	// Set CSV headers
+	ignore_user_abort( true );
+
+	if( !edd_is_func_disabled( 'set_time_limit' ) && !ini_get( 'safe_mode' ) )
+		set_time_limit( 0 );
+
+	nocache_headers();
+
+	header( 'Content-Type: text/csv; charset=utf-8' );
+	header( 'Content-Disposition: attachment;filename=edd-export-products-' . date( 'm-d-y' ) . '.csv' );
+	header( 'Expires: 0' );
+
+	// Generate the CSV
+	$csv = fopen( 'php://output', 'w' );
+
+	foreach( $data as $fields ) {
+		fputcsv( $csv, $fields );
+	}
+
+	fclose( $csv );
+}
+add_action( 'edd_export_csv', 'edd_export_csv' );
 
 
 /**
@@ -385,7 +543,7 @@ function edd_process_csv_import() {
 					'post_parent'		=> $post_id
 				);
 
-				$attachment_id = wp_insert_attachment( $attachment, $final_images[0]['path'], $post_id );
+				$attachment_id = wp_insert_attachment( $attachment, $final_images[0]['url'], $post_id );
 
 				if( !is_wp_error( $attachment_id ) && $attachment_id ) {
 					update_post_meta( $post_id, '_thumbnail_id', $attachment_id );
