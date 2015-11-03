d3c0161b9a0255ee8aeddb0a5873da4fa6ae9cb0
Pippin Williamson <pippin@pippinsplugins.com>
2015-11-02 21:41:57 -0600

    Version bump and changelog

--- edd-csv-manager.php
+++ edd-csv-manager.php
@@ -3,7 +3,7 @@
 Plugin Name:     Easy Digital Downloads - CSV Manager
 Plugin URI:      https://easydigitaldownloads.com/extensions/edd-csv-manager/
 Description:     Allows store owners to import products from or export products to a CSV file
-Version:         1.2.7
+Version:         1.2.8
 Author:          Daniel J Griffiths and Pippin Williamson
 Author URI:      https://easydigitaldownloads.com
 @package         EDD CSV Manager
